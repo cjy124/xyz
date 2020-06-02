0673c61fdac1abeb3ebbd652395496f6cc29d635
cjy124 <c.junyong@outlook.com>
2020-06-02 19:00:11 +0800

    clean up

--- src/App.js
+++ src/App.js
@@ -5,19 +5,6 @@ import { Button } from "@material-ui/core"
 import * as Yup from "yup";
 import { validateUEN } from './validateUEN'
 
-const dateNow = new Date();
-const year = dateNow.getFullYear(); // Getting current year from the created Date object
-const monthWithOffset = dateNow.getUTCMonth() + 1; // January is 0 by default in JS. Offsetting +1 to fix date for calendar.
-const month = // Setting current Month number from current Date object
-  monthWithOffset.toString().length < 2 // Checking if month is < 10 and pre-prending 0 to adjust for date input.
-    ? `0${monthWithOffset}`
-    : monthWithOffset;
-const date =
-  dateNow.getUTCDate().toString().length < 2 // Checking if date is < 10 and pre-prending 0 if not to adjust for date input.
-    ? `0${dateNow.getUTCDate()}`
-    : dateNow.getUTCDate();
-const mDateNow = `${date}-${month}-${year}`;
-
 const CustomTF = ({ label, ...props }) => {
   const [field,meta] = useField(props);
   const errorTxt = meta.error && meta.touched ? meta.error : '';
